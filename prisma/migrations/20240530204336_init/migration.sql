ee733cb052b0330cc2ac55071ec34d4ca0388e9c
Gabriel Bittencourt Camillo de Souzae <Gabrielbcds40@gmail.com>
2024-05-30 18:32:33 -0300

    commit

---
+++ prisma/migrations/20240530204336_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Produto" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "value" REAL NOT NULL,
+    "quantity" INTEGER NOT NULL
+);
